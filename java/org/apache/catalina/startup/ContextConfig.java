263e6f11a5d1393ec99df5d2729309ddd33b0529
Shing Wai Chan <shing.wai.chan@oracle.com>
2008-06-19 23:44:18 +0000

    port tomcat bug 29936 (commit 649974): XML parser loading problems by container reviewed by Jan Luehe
    svn path=/trunk/; revision=21095
    
    
    Former-commit-id: dd6e8ef5deeba52217ea8c4db8e1ae5c696ec195

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -358,10 +358,6 @@ public class ContextConfig
         
         long t1=System.currentTimeMillis();
 
-        if (webDigester == null){
-            webDigester = createWebDigester();
-        }
-        
         URL url=null;
         // Process the application web.xml file
         synchronized (webDigester) {
@@ -717,10 +713,6 @@ public class ContextConfig
             return;
         }
 
-        if (webDigester == null){
-            webDigester = createWebDigester();
-        }
-        
         // Process the default web.xml file
         synchronized (webDigester) {
             try {
@@ -825,9 +817,6 @@ public class ContextConfig
         }
         if (source == null)
             return;
-        if (contextDigester == null){
-            contextDigester = createContextDigester();
-        }
         synchronized (contextDigester) {
             try {
                 source.setByteStream(stream);
@@ -917,8 +906,17 @@ public class ContextConfig
      * Process a "init" event for this Context.
      */
     protected void init() {
-
         // Called from StandardContext.init()
+
+        if (webDigester == null){
+            webDigester = createWebDigester();
+            webDigester.getParser();
+        }
+        
+        if (contextDigester == null){
+            contextDigester = createContextDigester();
+            contextDigester.getParser();
+        }
 
         if (log.isLoggable(Level.FINE))
             log.fine(sm.getString("contextConfig.init"));
