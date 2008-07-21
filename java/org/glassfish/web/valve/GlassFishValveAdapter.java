d0035df781f027bd0858f60f2b3d8b5e06269050
Jan Luehe <jluehe@java.net>
2008-07-21 19:19:06 +0000

    Added support for GlassFish-style valves compiled against the old org.apache.catalina.Valve interface from GlassFish releases prior to V3 (which has been renamed to org.glassfish.web.valve.GlassFishValve in V3), to ensure binary compatibility of these valves.
    Tests run:
    - QL
    - valveGlassFishStyleBackwardCompatible and valveTomcatStyle devtests
    
    svn path=/trunk/; revision=21342
    
    
    Former-commit-id: c92b6b7c6107f3fa29a21c6fae8d91c12eb5a8af

---
+++ java/org/glassfish/web/valve/GlassFishValveAdapter.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ */
+
+package org.glassfish.web.valve;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import javax.servlet.ServletException;
+import org.apache.catalina.Request;
+import org.apache.catalina.Response;
+import org.apache.catalina.Valve;
+
+/**
+ * Adapter valve for wrapping a GlassFish-style valve that was compiled
+ * against the "old" org.apache.catalina.Valve interface from GlassFish
+ * releases prior to V3 (which has been renamed to
+ * org.glassfish.web.valve.GlassFishValve in GlassFish V3).
+ *
+ * @author jluehe
+ */
+public class GlassFishValveAdapter implements GlassFishValve {
+
+    // The wrapped GlassFish-style valve to which to delegate
+    private Valve gfValve;
+
+    private Method invokeMethod;
+    private Method postInvokeMethod;
+
+    /**
+     * Constructor.
+     *
+     * @param gfValve The GlassFish valve to which to delegate
+     */
+    public GlassFishValveAdapter(Valve gfValve) throws Exception {
+        this.gfValve = gfValve;
+        invokeMethod = gfValve.getClass().getMethod("invoke", Request.class,
+                                                    Response.class);
+        postInvokeMethod = gfValve.getClass().getMethod("postInvoke",
+                                                        Request.class,
+                                                        Response.class);
+    }
+
+    public String getInfo() {
+        return gfValve.getInfo();
+    }
+
+    /**
+     * Delegates to the invoke() of the wrapped GlassFish-style valve.
+     */
+    public int invoke(Request request,
+                      Response response)
+                throws IOException, ServletException {
+        try {
+            return ((Integer) invokeMethod.invoke(gfValve, request, response)).intValue();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    /**
+     * Delegates to the postInvoke() of the wrapped GlassFish-style valve.
+     */
+    public void postInvoke(Request request, Response response)
+                throws IOException, ServletException {
+        try {
+            postInvokeMethod.invoke(gfValve, request, response);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
