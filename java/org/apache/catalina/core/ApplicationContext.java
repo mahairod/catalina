4fe4ae90c1f5d6dee6b9ff24607cd8b5b257a0b2
Jan Luehe <jluehe@java.net>
2008-08-25 16:56:55 +0000

    Removed redundant import
    svn path=/trunk/; revision=22078
    
    
    Former-commit-id: af2b11939513cbcea209591af6a6eaa233d4b6d5

--- java/org/apache/catalina/core/ApplicationContext.java
+++ java/org/apache/catalina/core/ApplicationContext.java
@@ -29,7 +29,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
