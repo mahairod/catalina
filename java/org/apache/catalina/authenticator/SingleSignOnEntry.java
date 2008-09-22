a2ab659572ff892f7c85e3d9cc0cfdb1210832fc
Jan Luehe <jluehe@java.net>
2008-09-22 21:05:18 +0000

    Incremental fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=5015 ("org.apache.catalina.authenticator.SingleSignOn thread safety issue"):
    Consolidate the definitions of SingleSignOnEntry into a single place
    
    Reviewed by: jfarcand
    
    svn path=/trunk/; revision=22862
    
    
    Former-commit-id: d2544bc9c4678c6a514a7f36f40969a0bb8755ac

---
+++ java/org/apache/catalina/authenticator/SingleSignOnEntry.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ *
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.catalina.authenticator;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.Container;
+import org.apache.catalina.HttpRequest;
+import org.apache.catalina.HttpResponse;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Request;
+import org.apache.catalina.Response;
+import org.apache.catalina.Session;
+import org.apache.catalina.SessionEvent;
+import org.apache.catalina.SessionListener;
+import org.apache.catalina.authenticator.Constants;
+import org.apache.catalina.Realm;
+
+/**
+ * A private class representing entries in the cache of authenticated users.
+ */
+public class SingleSignOnEntry {
+
+    public String id = null;
+
+    public String authType = null;
+
+    public String password = null;
+
+    public Principal principal = null;
+
+    public Session sessions[] = new Session[0];
+
+    public String username = null;
+
+    public String realmName = null;
+
+    public long lastAccessTime;
+
+    public SingleSignOnEntry(String id, Principal principal, String authType,
+                             String username, String password,
+                             String realmName) {
+        super();
+        this.id = id;
+        this.principal = principal;
+        this.authType = authType;
+        this.username = username;
+        this.password = password;
+        this.realmName = realmName;
+        this.lastAccessTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Gets the id of this SSO entry.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Adds the given session to this SingleSignOnEntry if it does not
+     * already exist.
+     * 
+     * @return true if the session was added, false otherwise
+     */
+    public synchronized boolean addSession(SingleSignOn sso, Session session) {
+        for (int i = 0; i < sessions.length; i++) {
+            if (session == sessions[i])
+                return false;
+        }
+        Session results[] = new Session[sessions.length + 1];
+        System.arraycopy(sessions, 0, results, 0, sessions.length);
+        results[sessions.length] = session;
+        sessions = results;
+        session.addSessionListener(sso);
+
+        return true;
+    }
+
+    public synchronized void removeSession(Session session) {
+        Session[] nsessions = new Session[sessions.length - 1];
+        for (int i = 0, j = 0; i < sessions.length; i++) {
+            if (session == sessions[i])
+                continue;
+            nsessions[j++] = sessions[i];
+        }
+        sessions = nsessions;
+    }
+
+    public synchronized Session[] findSessions() {
+        return (this.sessions);
+    }
+
+}
