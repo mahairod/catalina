08b6f66ef6ef39992e1bc07631197a66791f1d09
Amy Moon <amy.roh@oracle.com>
2009-01-27 04:50:10 +0000

    Fix issue 7060 old bundled jar not removed during redeploy - close JarFile when finised.  Ran QL.
    svn path=/trunk/; revision=24465
    
    
    Former-commit-id: 83862a31085426df5da291af6f77fbdda47ff216

--- java/org/apache/catalina/startup/TldConfig.java
+++ java/org/apache/catalina/startup/TldConfig.java
@@ -663,6 +663,14 @@ public final class TldConfig  {
                     sm.getString("contextConfig.tldEntryException",
                                  name, uri.toString(), context.getPath()),
                     e);
+        } finally {
+            if (jarFile != null) {
+                try {
+                    jarFile.close();
+                } catch (Throwable t) {
+                    // Ignore
+                }
+            }
         }
     }
 
