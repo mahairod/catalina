5085d9335e306efd653e343f14ff8f96768b25a0
Jan Luehe <jluehe@java.net>
2009-02-27 20:42:29 +0000

    SessionCookieConfig related changes: - Turned SessionCookieConfig into an interface, and added setter methods - Removed ServletContext#setSessionCookieConfig
    Tests run:
    - QL
    - Servlet TCK
    
    svn path=/trunk/; revision=25012
    
    
    Former-commit-id: 10281f2358447c06a502582cafaff12b5dde71a4

---
+++ java/org/apache/catalina/core/SessionCookieConfigImpl.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ */
+
+package org.apache.catalina.core;
+
+import javax.servlet.*;
+import org.apache.catalina.util.StringManager;
+
+/**
+ * Class that may be used to configure various properties of cookies 
+ * used for session tracking purposes.
+ */
+public class SessionCookieConfigImpl implements SessionCookieConfig {
+
+    private static final StringManager sm =
+        StringManager.getManager(Constants.Package);
+
+    private String domain;
+    private String path;
+    private String comment;
+    private boolean httpOnly;
+    private boolean secure;
+    private StandardContext ctx;
+
+
+    /**
+     * Constructor
+     */
+    SessionCookieConfigImpl(StandardContext ctx) {
+        this.ctx = ctx;
+    }
+
+
+    /**
+     * @param name the cookie name to use
+     *
+     * @throws IllegalStateException if the <tt>ServletContext</tt>
+     * from which this <tt>SessionCookieConfig</tt> was acquired has
+     * already been initialized
+     */
+    public void setName(String name) {
+        if (ctx.isContextInitializedCalled()) {
+            throw new IllegalStateException(
+                sm.getString("sessionCookieConfig.alreadyInitialized",
+                             "name", ctx.getName()));
+        }
+
+        ctx.setSessionCookieName(name);
+        ctx.setSessionCookieConfigInitialized(true);
+    }
+
+
+    /**
+     * @return the cookie name set via {@link #setName}, or
+     * <tt>JSESSIONID</tt> if {@link #setName} was never called
+     */
+    public String getName() {
+        return ctx.getSessionCookieName();
+    }
+
+
+    /**
+     * @param domain the cookie domain to use
+     *
+     * @throws IllegalStateException if the <tt>ServletContext</tt>
+     * from which this <tt>SessionCookieConfig</tt> was acquired has
+     * already been initialized
+     */
+    public void setDomain(String domain) {
+        if (ctx.isContextInitializedCalled()) {
+            throw new IllegalStateException(
+                sm.getString("sessionCookieConfig.alreadyInitialized",
+                             "dnmain", ctx.getName()));
+        }
+
+        this.domain = domain;
+        ctx.setSessionCookieConfigInitialized(true);
+    }
+
+
+    /**
+     * @return the cookie domain set via {@link #setDomain}, or
+     * <tt>null</tt> if {@link #setDomain} was never called
+     */
+    public String getDomain() {
+        return domain;
+    }
+
+
+    /**
+     * @param path the cookie path to use
+     *
+     * @throws IllegalStateException if the <tt>ServletContext</tt>
+     * from which this <tt>SessionCookieConfig</tt> was acquired has
+     * already been initialized
+     */
+    public void setPath(String path) {
+        if (ctx.isContextInitializedCalled()) {
+            throw new IllegalStateException(
+                sm.getString("sessionCookieConfig.alreadyInitialized",
+                             "path", ctx.getName()));
+        }
+
+        this.path = path;
+        ctx.setSessionCookieConfigInitialized(true);
+    }
+
+
+    /**
+     * @return the cookie path set via {@link #setPath}, or the context
+     * path of the <tt>ServletContext</tt> from which this 
+     * <tt>SessionCookieConfig</tt> was acquired if {@link #setPath}
+     * was never called
+     */
+    public String getPath() {
+        return path;
+    }
+
+
+    /**
+     * @param comment the cookie comment to use
+     *
+     * @throws IllegalStateException if the <tt>ServletContext</tt>
+     * from which this <tt>SessionCookieConfig</tt> was acquired has
+     * already been initialized
+     */
+    public void setComment(String comment) {
+        if (ctx.isContextInitializedCalled()) {
+            throw new IllegalStateException(
+                sm.getString("sessionCookieConfig.alreadyInitialized",
+                             "comment", ctx.getName()));
+        }
+
+        this.comment = comment;
+        ctx.setSessionCookieConfigInitialized(true);
+    }
+
+
+    /**
+     * @return the cookie comment set via {@link #setComment}, or
+     * <tt>null</tt> if {@link #setComment} was never called
+     */
+    public String getComment() {
+        return comment;
+    }
+
+
+    /**
+     * @param httpOnly true if the session tracking cookies created
+     * on behalf of the <tt>ServletContext</tt> from which this
+     * <tt>SessionCookieConfig</tt> was acquired shall be marked as
+     * <i>HttpOnly</i>, false otherwise
+     *
+     * @throws IllegalStateException if the <tt>ServletContext</tt>
+     * from which this <tt>SessionCookieConfig</tt> was acquired has
+     * already been initialized
+     */
+    public void setHttpOnly(boolean httpOnly) {
+        if (ctx.isContextInitializedCalled()) {
+            throw new IllegalStateException(
+                sm.getString("sessionCookieConfig.alreadyInitialized",
+                             "httpOnly", ctx.getName()));
+        }
+
+        this.httpOnly = httpOnly;
+        ctx.setSessionCookieConfigInitialized(true);
+    }
+
+
+    /**
+     * @return true if the session tracking cookies created on behalf of the
+     * <tt>ServletContext</tt> from which this <tt>SessionCookieConfig</tt>
+     * was acquired will be marked as <i>HttpOnly</i>, false otherwise
+     */
+    public boolean isHttpOnly() {
+        return httpOnly;
+    }
+
+
+    /**
+     * @param secure true if the session tracking cookies created on
+     * behalf of the <tt>ServletContext</tt> from which this
+     * <tt>SessionCookieConfig</tt> was acquired shall be marked as
+     * <i>secure</i> even if the request that initiated the corresponding
+     * session is using plain HTTP instead of HTTPS, and false if they
+     * shall be marked as <i>secure</i> only if the request that initiated
+     * the corresponding session was also secure
+     *
+     * @throws IllegalStateException if the <tt>ServletContext</tt>
+     * from which this <tt>SessionCookieConfig</tt> was acquired has
+     * already been initialized
+     */
+    public void setSecure(boolean secure) {
+        if (ctx.isContextInitializedCalled()) {
+            throw new IllegalStateException(
+                sm.getString("sessionCookieConfig.alreadyInitialized",
+                             "secure", ctx.getName()));
+        }
+
+        this.secure = secure;
+        ctx.setSessionCookieConfigInitialized(true);
+    }
+
+
+    /**
+     * @return true if the session tracking cookies created on behalf of the
+     * <tt>ServletContext</tt> from which this <tt>SessionCookieConfig</tt>
+     * was acquired will be marked as <i>secure</i> even if the request
+     * that initiated the corresponding session is using plain HTTP
+     * instead of HTTPS, and false if they will be marked as <i>secure</i>
+     * only if the request that initiated the corresponding session was
+     * also secure
+     */
+    public boolean isSecure() {
+        return secure;
+    }
+}
