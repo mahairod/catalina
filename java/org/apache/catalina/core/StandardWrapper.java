7eaff6907425980934d9bf6ded47e5637ac0c678
Jan Luehe <jluehe@java.net>
2009-03-11 23:23:08 +0000

    Changed return type of getInitAttributeNames from Enumeration to Iterator
    svn path=/trunk/; revision=25318
    
    
    Former-commit-id: 680fa6f500d5610be15ea9024f6430dfada36f48

--- java/org/apache/catalina/core/StandardWrapper.java
+++ java/org/apache/catalina/core/StandardWrapper.java
@@ -1706,9 +1706,9 @@ public class StandardWrapper
     }
 
 
-    public Enumeration<String> getInitAttributeNames() {
+    public Iterator<String> getInitAttributeNames() {
         synchronized (attributes) {
-            return (new Enumerator(attributes.keySet()));
+            return attributes.keySet().iterator();
         }
     }
 
