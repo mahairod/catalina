2ea9a95ee4f40a5b8febd92319c336a6305e66eb
Jan Luehe <jluehe@java.net>
2009-03-26 03:10:05 +0000

    Added support for ServletContext#create[Servlet|Filter] (Servlet 3.0 feature)
    Tests run: QL
    
    svn path=/trunk/; revision=25666
    
    
    Former-commit-id: 55db721c48c491c12d33d06c8fdf8ab9d6c1cb11

--- java/org/apache/catalina/core/StandardWrapper.java
+++ java/org/apache/catalina/core/StandardWrapper.java
@@ -1335,7 +1335,7 @@ public class StandardWrapper
         }
 
         try {
-            instanceSupport.fireInstanceEvent(BEFORE_INIT_EVENT,servlet);
+            instanceSupport.fireInstanceEvent(BEFORE_INIT_EVENT, servlet);
             // START SJS WS 7.0 6236329
             //if( System.getSecurityManager() != null) {
             if ( SecurityUtil.executeUnderSubjectDoAs() ){
