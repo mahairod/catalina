8bac58950ce8ca1492964b81e10d7f9e2a4d8267
Rajiv Mordani <mode@java.net>
2009-04-17 20:52:20 +0000

    Stubbed implementation for fileupload
    svn path=/trunk/; revision=26342
    
    
    Former-commit-id: 8bf4d1d9f73e8697b3916d91c249ce5e1d88f7c9

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -3968,6 +3968,16 @@ public class Request
         return startAsync(servletRequest, servletResponse, false);
     }
 
+    public Iterable<Part> getParts() {
+        //TODO: Stub out the implementation
+        return null; 
+    }
+
+    public Part getPart(String name) {
+        //TODO: Stub out the implementation
+        return null;
+    }
+
 
     /**
      * Starts async processing on this request.
