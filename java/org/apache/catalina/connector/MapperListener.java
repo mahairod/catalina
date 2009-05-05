8d4d8e58a0f46b9e258865af87bce9d9a0dd5a55
Dies Koper <dkoper@java.net>
2009-05-05 03:18:53 +0000

    Issue number: 7990 Submitted by: Dies Koper Reviewed by:  Jan Luehe Tests run:    QL
    Added code to filter out MBeans belonging to a different server instance than the server instance in which this MapperListener instance is running.
    
    
    svn path=/trunk/; revision=26977
    
    
    Former-commit-id: 22d25fc4b857aa30feb254b09e5f3bfae4c7b4ce

--- java/org/apache/catalina/connector/MapperListener.java
+++ java/org/apache/catalina/connector/MapperListener.java
@@ -181,7 +181,7 @@ public class MapperListener
 
 
             // Query contexts
-            onStr = domain + ":j2eeType=WebModule,*";
+            onStr = domain + ":j2eeType=WebModule,*,J2EEServer=" + myInstance;
             objectName = new ObjectName(onStr);
             set = mBeanServer.queryMBeans(objectName, null);
             iterator = set.iterator();
@@ -191,7 +191,7 @@ public class MapperListener
             }
 
             // Query wrappers
-            onStr = domain + ":j2eeType=Servlet,*";
+            onStr = domain + ":j2eeType=Servlet,*,J2EEServer=" + myInstance;
             objectName = new ObjectName(onStr);
             set = mBeanServer.queryMBeans(objectName, null);
             iterator = set.iterator();
