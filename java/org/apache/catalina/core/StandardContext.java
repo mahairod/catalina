4ee48295f784671f5fb6f1f567011c93c37ab9d6
Jan Luehe <jluehe@java.net>
2009-07-23 23:18:18 +0000

    Added ServletContext#getActual[Major|Minor]Version, which returns the [major|minor] version of the Servlet specification that the web application represented by the ServletContext is based on
    svn path=/trunk/; revision=29655
    
    
    Former-commit-id: daae474f6bc0e8c7cd946b697ffd81c2e3d2073a

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -699,6 +699,12 @@ public class StandardContext
 
     private Map<Class<?>, ArrayList<Class<? extends ServletContainerInitializer>>> servletContainerInitializerInterestList = null;
 
+    // The major Servlet spec version of the web.xml
+    private int actualMajorVersion = 0;
+
+    // The minor Servlet spec version of the web.xml
+    private int actualMinorVersion = 0;
+
     // ----------------------------------------------------- Context Properties
 
     public String getEncodedPath() {
@@ -2743,6 +2749,22 @@ public class StandardContext
         }
         mySecurityManager.checkGetClassLoaderPermission(webappLoader);
         return webappLoader;
+    }
+
+    public void setActualMajorVersion(int actualMajorVersion) {
+        this.actualMajorVersion = actualMajorVersion;
+    }
+
+    public int getActualMajorVersion() {
+        return actualMajorVersion;
+    }
+
+    public void setActualMinorVersion(int actualMinorVersion) {
+        this.actualMinorVersion = actualMinorVersion;
+    }
+    
+    public int getActualMinorVersion() {
+        return actualMinorVersion;
     }
 
     /**
