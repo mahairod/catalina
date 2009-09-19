d9d558ea55cd4bc291e52715a4d7a3c9e8d82398
Jan Luehe <jluehe@java.net>
2009-09-19 00:49:26 +0000

    Incremental fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=9550 ("[PERFORMANCE] Pass appName to request[Start|End]Event probe events")
    Tests run: QL
    
    svn path=/trunk/; revision=31558
    
    
    Former-commit-id: 5472f7d56ef0ca2908ba85e8f49548aed9b1af09

--- java/org/apache/catalina/connector/Connector.java
+++ java/org/apache/catalina/connector/Connector.java
@@ -35,7 +35,6 @@ import javax.management.MBeanServer;
 import javax.management.MBeanRegistration;
 import javax.management.MalformedObjectNameException;
 
-
 import org.apache.tomcat.util.modeler.Registry;
 
 import com.sun.grizzly.util.IntrospectionUtils;
@@ -46,6 +45,7 @@ import com.sun.grizzly.tcp.ProtocolHandler;
 
 import org.apache.catalina.Container;
 import org.apache.catalina.Context;
+import org.apache.catalina.Host;
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleListener;
@@ -1400,12 +1400,11 @@ public class Connector
      * been entered the web container.
      *
      * @param request the request object
+     * @param host the virtual server to which the request was mapped
      * @param context the Context to which the request was mapped
-     * @param hostName the name of the virtual server to which the request
-     * was mapped
      */
-    public void requestStartEvent(HttpServletRequest request, Context context,
-                                  String hostName) {
+    public void requestStartEvent(HttpServletRequest request, Host host,
+            Context context) {
         // Deliberate noop
     };
 
@@ -1414,13 +1413,12 @@ public class Connector
      * to exit from the web container.
      *
      * @param request the request object
+     * @param host the virtual server to which the request was mapped
      * @param context the Context to which the request was mapped
-     * @param hostName the name of the virtual server to which the request
-     * was mapped
      * @param statusCode the response status code
      */
-    public void requestEndEvent(HttpServletRequest request, Context context,
-                                String hostName, int statusCode) {
+    public void requestEndEvent(HttpServletRequest request, Host host,
+            Context context, int statusCode) {
         // Deliberate noop
     };
 
