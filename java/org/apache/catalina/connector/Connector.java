cf258a4ad23fc374e4624f0d1bd76e5cfc37e690
Jerome Dochez <dochez@java.net>
2009-10-07 05:39:25 +0000

    Startup performance improvement checkin. Upgrade hk2 to latest version with lazy initialization of the transaction subsystem. added an orb-enabler to avoid loading any ORB related classes until necessary. moved MEJB to new javaee-kernel module (more will move there later) to avoid loading EJB container modules made LocalPassword a contract and moved the class in kernel. reworked AdminAuthenticator so it's instancitated when an authentication is actually necessary. rewoked transaction recovery so it's not loaded if no app is deployed (to be continued with Marina) make JMX/AMX use a proxy pattern for authentication of requests (admin authenticator needs security which needs a lot of modules).
    Ran all QL tests
     
    
    svn path=/trunk/; revision=32355
    
    
    Former-commit-id: 101cae77aefbe6035b050f57893d76bd436967ff

--- java/org/apache/catalina/connector/Connector.java
+++ java/org/apache/catalina/connector/Connector.java
@@ -25,7 +25,6 @@ import java.lang.reflect.Constructor;
 import java.net.URLEncoder;
 import java.util.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 // START OF SJSAS 8.1 PE 6191830
 import java.security.cert.X509Certificate;
@@ -42,6 +41,7 @@ import com.sun.grizzly.util.http.mapper.Mapper;
 
 import com.sun.grizzly.tcp.Adapter;
 import com.sun.grizzly.tcp.ProtocolHandler;
+import com.sun.enterprise.glassfish.web.*;
 
 import org.apache.catalina.Container;
 import org.apache.catalina.Context;
@@ -58,12 +58,9 @@ import org.apache.catalina.net.ServerSocketFactory;
 import org.apache.catalina.util.LifecycleSupport;
 import org.apache.catalina.util.StringManager;
 // START S1AS 6188932
-import com.sun.appserv.ProxyHandler;
 // END S1AS 6188932
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jvnet.hk2.annotations.Inject;
-
 
 
 /**
