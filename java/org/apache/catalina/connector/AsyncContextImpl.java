5d78c348c7dadaeef32ad6f9aee6b3fc4f122249
Jan Luehe <jluehe@java.net>
2009-10-14 21:19:47 +0000

    Reinitialize AsyncContext with container's default async timeout
    svn path=/trunk/; revision=32761
    
    
    Former-commit-id: 0deb9659bc1f0934101b29984063d21439ff2bc3

--- java/org/apache/catalina/connector/AsyncContextImpl.java
+++ java/org/apache/catalina/connector/AsyncContextImpl.java
@@ -268,6 +268,7 @@ public class AsyncContextImpl implements AsyncContext {
         this.isOriginalRequestAndResponse = isOriginalRequestAndResponse;
         isDispatchInProgress.set(false);
         setOkToConfigure(true);
+        setTimeout(DEFAULT_ASYNC_TIMEOUT_MILLIS);
         notifyAsyncListeners(AsyncEventType.START_ASYNC, null);
         if (isOriginalRequestAndResponse) {
             zeroArgDispatchTarget = getZeroArgDispatchTarget(origRequest);
