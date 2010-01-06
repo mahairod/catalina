8125c4f5beef39f9b63dbfd23308a4bde769404f
Jan Luehe <jluehe@java.net>
2010-01-06 19:51:50 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=11333 ("When useBundledJsf is defined in sun-web.xml, the Mojarra ServletContainerInitializer shouldn't be invoked")
    svn path=/trunk/; revision=35259
    
    
    Former-commit-id: eb53073193fe4ca6db9970aa2729561b597902c3

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -5239,6 +5239,11 @@ public class StandardContext
         try {
             for (Class<? extends ServletContainerInitializer> initializer :
                     initializerList.keySet()) {
+                // See IT 11333
+                if (isUseMyFaces() &&
+                        Globals.FACES_INITIALIZER.equals(initializer.getName())) {
+                    continue;
+                }
                 try {
                     ServletContainerInitializer iniInstance =
                         initializer.newInstance();
