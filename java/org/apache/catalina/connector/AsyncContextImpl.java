72de7d398489608c7761b82d14c11741e859568a
Ryan Lubke <ryan.lubke@oracle.com>
2010-07-28 20:28:17 +0000

    - Integrate Grizzly 1.9.19-beta7 - Include glassfish-side of fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=11739.
    svn path=/trunk/; revision=39171
    
    
    Former-commit-id: 62db8be53f75db2d943599f01301a7ced50d0675

--- java/org/apache/catalina/connector/AsyncContextImpl.java
+++ java/org/apache/catalina/connector/AsyncContextImpl.java
@@ -59,11 +59,18 @@ public class AsyncContextImpl implements AsyncContext {
     // defaults to false
     private AtomicBoolean isDispatchInProgress = new AtomicBoolean(); 
 
+    private ThreadLocal<Boolean> isDispatchInScope = new ThreadLocal<Boolean>() {
+        @Override
+        protected Boolean initialValue() {
+            return Boolean.FALSE;
+        }
+    };
+
     private AtomicBoolean isOkToConfigure = new AtomicBoolean(true); 
 
     private long asyncTimeoutMillis = DEFAULT_ASYNC_TIMEOUT_MILLIS;
 
-    private LinkedList<AsyncListenerContext> asyncListenerContexts =
+    private final LinkedList<AsyncListenerContext> asyncListenerContexts =
         new LinkedList<AsyncListenerContext>();
 
     // The number of times this AsyncContext has been reinitialized via a call
@@ -133,6 +140,7 @@ public class AsyncContextImpl implements AsyncContext {
         }
         ApplicationDispatcher dispatcher = (ApplicationDispatcher)
             servletRequest.getRequestDispatcher(zeroArgDispatchTarget);
+        isDispatchInScope.set(true);
         if (dispatcher != null) {
             if (isDispatchInProgress.compareAndSet(false, true)) {
                 pool.execute(new Handler(this, dispatcher, origRequest));
@@ -155,6 +163,7 @@ public class AsyncContextImpl implements AsyncContext {
         }
         ApplicationDispatcher dispatcher = (ApplicationDispatcher)
             servletRequest.getRequestDispatcher(path);
+        isDispatchInScope.set(true);
         if (dispatcher != null) {
             if (isDispatchInProgress.compareAndSet(false, true)) {
                 pool.execute(new Handler(this, dispatcher, origRequest));
@@ -177,6 +186,7 @@ public class AsyncContextImpl implements AsyncContext {
         }
         ApplicationDispatcher dispatcher = (ApplicationDispatcher)
             context.getRequestDispatcher(path);
+        isDispatchInScope.set(true);
         if (dispatcher != null) {
             if (isDispatchInProgress.compareAndSet(false, true)) {
                 pool.execute(new Handler(this, dispatcher, origRequest));
@@ -190,6 +200,16 @@ public class AsyncContextImpl implements AsyncContext {
             log.warning("Unable to acquire RequestDispatcher for " + path +
                         "in servlet context " + context.getContextPath());
         }
+    }
+
+    boolean isDispatchInScope() {
+        return isDispatchInScope.get();
+    }
+
+    boolean getAndResetDispatchInScope() {
+        final boolean flag = isDispatchInScope.get();
+        isDispatchInScope.set(Boolean.FALSE);
+        return flag;
     }
 
     @Override
@@ -239,6 +259,7 @@ public class AsyncContextImpl implements AsyncContext {
         }
     }
 
+    @Override
     public <T extends AsyncListener> T createListener(Class<T> clazz)
             throws ServletException {
         T listener = null;
@@ -339,6 +360,7 @@ public class AsyncContextImpl implements AsyncContext {
             this.origRequest = origRequest;
         }
        
+        @Override
         public void run() {
             asyncContext.isStartAsyncInScope.set(Boolean.TRUE);
             origRequest.setAttribute(Globals.DISPATCHER_TYPE_ATTR,
