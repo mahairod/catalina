83618b587300c578a2e8ff6354215bde52342f74
Shing Wai Chan <shing.wai.chan@oracle.com>
2010-08-05 23:06:05 +0000

    port Tomcat change http://svn.apache.org/viewvc?view=revision&revision=883203 ("Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=482   Use volatile since field is accessed by multiple threads")
    svn path=/trunk/; revision=39434
    
    
    Former-commit-id: 8d4514e407645113518df9aae122187a47911536

--- java/org/apache/catalina/core/ContainerBase.java
+++ java/org/apache/catalina/core/ContainerBase.java
@@ -264,7 +264,7 @@ public abstract class ContainerBase
     /**
      * The background thread completion semaphore.
      */
-    private boolean threadDone = false;
+    private volatile boolean threadDone = false;
 
 
     /**
