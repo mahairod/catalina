d29191466c0c98f4e7e51b777609a5fa36f911d1
Shing Wai Chan <shing.wai.chan@oracle.com>
2011-03-15 16:29:10 +0000

    fix FindBugs issue: DLS_DEAD_LOCAL_STORE
    svn path=/trunk/; revision=45567
    
    
    Former-commit-id: db032d8d9e285c9f9f0ce87a11430892add829e4

--- java/org/apache/catalina/authenticator/SingleSignOn.java
+++ java/org/apache/catalina/authenticator/SingleSignOn.java
@@ -416,7 +416,7 @@ public class SingleSignOn
         // see if we are the last session, if so blow away ssoId
         if (sso.isEmpty()) {
             synchronized (cache) {
-                sso = cache.remove(ssoId);
+                cache.remove(ssoId);
             }
         }
     }
