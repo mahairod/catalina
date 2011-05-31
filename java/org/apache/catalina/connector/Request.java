e15092a7656e2c982230d3ffe167ec51e74aa270
Ryan Lubke <ryan.lubke@oracle.com>
2011-05-31 19:42:56 +0000

    [FINDBUGS]  web/web-core/src/main/java/org/apache/catalina/connector/Request.java:4032: DLS_DEAD_LOCAL_STORE: Dead store to asyncContextFinal in org.apache.catalina.connector.Request.startAsync(ServletRequest, ServletResponse, boolean)
    svn path=/trunk/; revision=47214
    
    Former-commit-id: 1bafcd9c6dab2ef2df7a6259fdf6dc1de96adfc8

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -3856,7 +3856,9 @@ public class Request
                     sm.getString("request.startAsync.notSupported"));
         }
 
-        if (asyncContext != null) {
+        final AsyncContextImpl asyncContextLocal = asyncContext;
+
+        if (asyncContextLocal != null) {
             if (isAsyncStarted()) {
                 throw new IllegalStateException(
                         sm.getString("request.startAsync.alreadyCalled"));
@@ -3865,18 +3867,22 @@ public class Request
                 throw new IllegalStateException(
                         sm.getString("request.startAsync.alreadyComplete"));
             }
-            if (!asyncContext.isStartAsyncInScope()) {
+            if (!asyncContextLocal.isStartAsyncInScope()) {
                 throw new IllegalStateException(
                         sm.getString("request.startAsync.notInScope"));
             }
 
             // Reinitialize existing AsyncContext
-            asyncContext.reinitialize(servletRequest, servletResponse,
+            asyncContextLocal.reinitialize(servletRequest, servletResponse,
                     isOriginalRequestAndResponse);
         } else {
-            asyncContext = new AsyncContextImpl(this, servletRequest,
-                    (Response) getResponse(), servletResponse,
-                    isOriginalRequestAndResponse);
+            final AsyncContextImpl asyncContextFinal =
+                    new AsyncContextImpl(this,
+                                         servletRequest,
+                                         (Response) getResponse(),
+                                         servletResponse,
+                                         isOriginalRequestAndResponse);
+            asyncContext = asyncContextFinal;
 
             CompletionHandler requestCompletionHandler =
                 new CompletionHandler<Request>() {
