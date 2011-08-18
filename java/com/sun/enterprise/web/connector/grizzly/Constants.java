6edf5ecdc67a5a872e22781a679851f558a5e6f2
Oleksiy Stashok <oleksiy.stashok@oracle.com>
2011-08-18 12:14:29 +0000

    + stop using the localization bundle from grizzly module
    svn path=/trunk/; revision=48899
    
    
    Former-commit-id: 80345e05246cfdcbc491fd2c8d7b6f39e0946daa

---
+++ java/com/sun/enterprise/web/connector/grizzly/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ */
+
+package com.sun.enterprise.web.connector.grizzly;
+
+/**
+ * Constants for the <code>com.sun.enterprise.web.connector.grizzly</code>
+ * package.
+ */
+
+public final class Constants {
+
+    public static final String Package = "com.sun.enterprise.web.connector.grizzly";
+}
