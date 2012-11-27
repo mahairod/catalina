fc06b6f465b054eb389950e4d7b3f185740719f8
Shing Wai Chan <shing.wai.chan@oracle.com>
2012-11-27 22:15:04 +0000

    fix issue http://java.net/jira/browse/GLASSFISH-19364 ("web container does not call AsyncContext.complete() if dispatched servlet throws Exception")
    svn path=/trunk/; revision=57176
    
    
    Former-commit-id: ae692e4392eaf29f0ae171d42141a2f25e0c5f1d

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -4430,7 +4430,7 @@ public class Request
          */
         if (asyncContext != null
                 && !asyncContext.isDispatchInScope()
-                && !isAsyncComplete && isAsyncStarted()) {
+                && !isAsyncComplete) {
             ((HttpServletResponse) response).setStatus(
                 HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.setError();
@@ -4449,7 +4449,7 @@ public class Request
                  * did not call AsyncContext#complete or any of the
                  * AsyncContext#dispatch methods, call AsyncContext#complete
                  */
-                if (!isAsyncComplete && isAsyncStarted()) {
+                if (!isAsyncComplete) {
                     asyncComplete();
                 }
             }
