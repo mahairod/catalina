301f0a78ff5e4ef769b7877768aec91ce0603505
Shing Wai Chan <shing.wai.chan@oracle.com>
2013-02-14 23:38:48 +0000

    fix the implementation for http://java.net/jira/browse/SERVLET_SPEC-54 ("Javadoc inconsistent for HttpServletRequest#changeSessionId()")
    svn path=/trunk/; revision=59508
    
    
    Former-commit-id: c4bafabef50c16a7f51f796da41cd511ebad19c6

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -3108,7 +3108,7 @@ public class Request
      * Change the session id of the current session associated with this
      * request and return the new session id. 
      *
-     * @return the original session id
+     * @return the new session id
      *
      * @throws IllegalStateException if there is no session associated
      * with the request
@@ -3145,7 +3145,7 @@ public class Request
             ((HttpResponse)response).addSessionCookieInternal(newCookie);
         }
 
-        return oldSessionId;
+        return newSessionId;
     }
 
     /**
