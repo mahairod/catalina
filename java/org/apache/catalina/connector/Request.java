661d9cb113379961a956acb8abb5c6378dc09df9
Shing Wai Chan <shing.wai.chan@oracle.com>
2013-03-25 19:27:13 +0000

    fix issue http://java.net/jira/browse/GLASSFISH-19473 ("Exception thrown in the server.log while testing servlet new   method onError() in ServletInputStream class")
    svn path=/trunk/; revision=60774
    
    
    Former-commit-id: 9a3e09f1f53336c66c66eba5b6a342aecd02a4fa

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -4388,6 +4388,10 @@ public class Request
             asyncContext.notifyAsyncListeners(
                     AsyncContextImpl.AsyncEventType.TIMEOUT, null);
         }
+        inputBuffer.disableReadHandler();
+        if (response instanceof Response) {
+            ((Response)response).disableWriteHandler();
+        }
         errorDispatchAndComplete(null);
     }
 
--- java/org/apache/catalina/connector/Response.java
+++ java/org/apache/catalina/connector/Response.java
@@ -2126,5 +2126,9 @@ public class Response
     public void setUpgrade(boolean upgrade) {
         this.upgrade = upgrade;
     }
+
+    void disableWriteHandler() {
+        outputBuffer.disableWriteHandler();
+    }
 }
 
