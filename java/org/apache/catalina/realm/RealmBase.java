a21cec840b4fca6523ce6b2d51559342c8b2680a
Shing Wai Chan <shing.wai.chan@oracle.com>
2013-04-04 00:05:27 +0000

    GLASSFISH-18356 - HttpServletRequest.login does not work correctly with single sign on (SSO) JSESSIONIDSSO cookie is not sent. Approved/reviewed by Ron Monzillo Passed QL, webtier dev tests, Servlet 3.0 TCK
    svn path=/trunk/; revision=61154
    
    
    Former-commit-id: 2ab80ffb06b1419c1db12c9d0ee68fee01ff5723

--- java/org/apache/catalina/realm/RealmBase.java
+++ java/org/apache/catalina/realm/RealmBase.java
@@ -1427,7 +1427,7 @@ public abstract class RealmBase
     }
 
     @Override
-    public void logout(HttpServletRequest hreq, HttpServletResponse hres) {
+    public void logout(HttpRequest hreq) {
         // no-op
     }
 
